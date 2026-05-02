a76228791b44881942981f488cf31868fcdabd48
Pedro <pedrocarneiromneto@gmail.com>
2026-05-01 22:41:49 -0300

    Migrate to native tailwind + vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import pkg from './package.json';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   base: './',
   define: {
     __APP_VERSION__: JSON.stringify(pkg.version),
